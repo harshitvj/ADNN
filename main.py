c47ebef1c3366b8aac191f890c5213d8f974151c
Harshit Vijaywargiya <37896373+harshitvj@users.noreply.github.com>
2018-04-02 14:36:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ frame = 0
 #     print("File doesn't exist, creating new file")
 #     training_data = np.array([])
 
+# Training Data Variable
 data = pd.DataFrame()
 
 while True:
@@ -46,10 +47,12 @@ while True:
     #     file1.write(str(output))
     #     file1.write("\n")
 
+    # Pandas DataFrames for x_data and y_data for Training Data
     dfx = pd.DataFrame(data_vector)
     dfy = pd.DataFrame(output)
-    dfz = pd.concat([dfx,dfy])
-    data = pd.concat([data,dfz], axis=1)
+    # dfz = pd.concat([dfx,dfy])
+    # data = pd.concat([data,dfz], axis=1)
+    data = pd.concat([data,pd.concat([dfx,dfy])], axis=1)
 
     cv2.imshow('Lanes', cropped_image)
 
@@ -58,4 +61,5 @@ while True:
         cv2.destroyAllWindows()
         break
 
+# Training Data to a CSV file. Each row of file is a training example of type (x1,x2,...,y1,y2,y3,y4)
 data.T.to_csv('data.csv',mode='a',header=False)
